311764286adcf1fc03de2274a3dee4b8b6989f14
yonatanh <yonatanh@jfrog.com>
2024-05-03 15:19:13 +0300

    psql connection modified

--- data/DB/PsqlConnection.py
+++ data/DB/PsqlConnection.py
@@ -1,15 +1,17 @@
 from sqlalchemy import create_engine
 import os
 
-PSQL_USERNAME=os.environ.get('PSQL_USERNAME')
-PSQL_PASSWORD=os.environ.get('PSQL_PASSWORD')
-PSQL_URL=os.environ.get('PSQL_URL')
+def PSQLConnection():
+    PSQL_USERNAME=os.environ.get('PSQL_USERNAME')
+    PSQL_PASSWORD=os.environ.get('PSQL_PASSWORD')
+    PSQL_URL=os.environ.get('PSQL_URL')
 
-engine = create_engine(f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@{PSQL_URL}/postgres")
+    engine = create_engine(f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@{PSQL_URL}/postgres")
 
-try:
-    with engine.connect() as connection_str:
-        print('Successfully connected to the PostgreSQL database')
-except Exception as e:
-    print(f'Failed to connect: {e}')
+    try:
+        with engine.connect() as connection_str:
+            print('Successfully connected to the PostgreSQL database')
+        return engine
+    except Exception as e:
+        print(f'Failed to connect: {e}')
     
