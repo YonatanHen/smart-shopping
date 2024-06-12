0266082f0d8652b64de796a44f421e5aca27309b
yonatanh <yonatanh@jfrog.com>
2024-06-12 23:25:28 +0300

    Adjusted the decision tree algorithm to meet the SQL format instead of the previous CSV

--- server/ML/fetch.py
+++ server/ML/fetch.py
@@ -33,7 +33,7 @@ def get_products():
     
     return products_data
 
-def get_list():
+def get_lists():
     Session = sessionmaker(bind=engine)
     
     session = Session()
@@ -51,7 +51,10 @@ def get_list():
     return list_data
 
 def create_new_list():
-    data = get_products()
+    products_data = get_products()
+    lists_data = get_lists()
+    
+    data = pd.merge(products_data, lists_data, left_on='list', right_on='list_id')
     
     #Get last date when each item was purchased
     max_purchase_date = data.groupby(pd.Grouper(key='item_name')).date.max().reset_index(name='last_purchased')
@@ -63,6 +66,9 @@ def create_new_list():
     df = pd.merge(max_purchase_date,frequency_of_items,on='item_name')
     df = pd.merge(df, average_purchase_interval, on='item_name')
 
+    df['count'].fillna(0, inplace=True)
+    df['avg_interval'].fillna(pd.Timedelta(seconds=0), inplace=True)
+    
     df['last_purchased'] = pd.to_datetime(df['last_purchased'])
     df['avg_interval'] = pd.to_timedelta(df['avg_interval']).dt.total_seconds()
     df['last_purchased'] = pd.to_timedelta(df['avg_interval']).dt.total_seconds()
@@ -75,9 +81,7 @@ def create_new_list():
     X = df[['count', 'avg_interval', 'last_purchased']]  # Features
     y = df['item_name']  # Target variable
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-
-    print(X_train.shape)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
     dtc = DecisionTreeClassifier()
 
