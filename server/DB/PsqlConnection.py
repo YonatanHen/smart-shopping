3806dc2f8ff6ae90a86027023dc64fd39b614c52
YehonatanHen <yonatan.h333@gmail.com>
2024-09-08 12:12:06 +0300

    edited connection

--- server/DB/PsqlConnection.py
+++ server/DB/PsqlConnection.py
@@ -1,8 +1,10 @@
 import os
 from sqlalchemy import create_engine
+from models import Base
 
 PSQL_USERNAME=os.environ.get('PSQL_USERNAME')
 PSQL_PASSWORD=os.environ.get('PSQL_PASSWORD')
 PSQL_URL=os.environ.get('PSQL_URL')
 
 engine = create_engine(f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@{PSQL_URL}")
+Base.metadata.create_all(bind=engine)
