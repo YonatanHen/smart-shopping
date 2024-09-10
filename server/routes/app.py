54b7ac336f040ab727f1d95daf78dd71bdc35e51
YehonatanHen <yonatan.h333@gmail.com>
2024-09-10 15:29:10 +0300

    Delete product from list API addede

--- server/routes/app.py
+++ server/routes/app.py
@@ -40,9 +40,14 @@ def products_name_id_api(product_name, id):
     if request.method == 'DELETE':
         try:
             updated_list = delete_product(product_name, id)
+            print(updated_list)
+            if updated_list is None:
+                raise ValueError('Product not found')
+            return jsonify(updated_list.to_dict(orient='records'))
+        except ValueError as e:
+            return jsonify({'error': str(e)}), 404
         except Exception as e:
-            error_message = str(e)
-            return jsonify({'error': error_message}), 500
+            return jsonify({'error': str(e)}), 500
 
 @app.route('/list', methods=['GET', 'POST'])
 def lists_api():
--- server/services/product_service.py
+++ server/services/product_service.py
@@ -2,14 +2,15 @@ import pandas as pd
 import warnings
 import sys
 import os
+from sqlalchemy import func, and_
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from DB.PsqlConnection import engine
-from .utils.create_db_session import get_session
 from models import Product
-
+from .utils.errors.sqlalchemy_error import handle_sqlalchemy_error
+from .utils.create_db_session import get_session
+from DB.PsqlConnection import engine
 
 # do not show warnings
 warnings.filterwarnings("ignore")
@@ -32,53 +33,60 @@ def get_all_products():
 
     return products_data
 
+
 def get_products_by_list_id(list_id, session=None):
     try:
         if session is None:
             session = get_session()
-                        
+
         res = session.query(Product).filter_by(list_id=list_id).all()
-        print(res)
-        
+
         products_data = pd.DataFrame([{
             'item_name': item.item_name,
             'amount': item.amount
         } for item in res])
-        
-        return products_data
-    
+
     except SQLAlchemyError as e:
         handle_sqlalchemy_error(e)
     except Exception as e:
         raise Exception("An unexpected error occurred") from e
     finally:
-        if os.getenv("ENV")=="Production":
+        if os.getenv("ENV") == "Production":
             session.close()
+
+        return products_data
+
 
 def delete_product(product_name, list_id, session=None):
     try:
         if session is None:
             session = get_session()
-        
-        product = session.query(Product).filter_by(item_name=product_name, list_id=list_id).first()
-        if not product:
-            raise ValueError(f"No product found with name '{product_name}' and list_id '{list_id}' found.")
-        
-        product_to_delete = session.query(Product).filter(list_id == Product.list_id and product_name == Product.item_name).first()
+
+        product_to_delete = session.query(Product).filter(
+            and_(
+                list_id == Product.list_id,
+                func.lower(product_name) == func.lower(Product.item_name)
+            )
+        ).first()
+
+        if product_to_delete is None:
+            return None
+
         session.delete(product_to_delete)
-        
         session.commit()
         
-        updated_list = session.query
+        updated_list = get_products_by_list_id(list_id, session)
+        return updated_list
+    
     except ValueError as e:
         raise
-    except Exception as e:  
-        raise Exception("An unexpected error occurred") from e     
-    
+    except SQLAlchemyError as e:
+        handle_sqlalchemy_error(e)
+    except Exception as e:
+        raise Exception("An unexpected error occurred") from e
+
     finally:
-        
-        if os.getenv("ENV")=="Production":
+        if os.getenv("ENV") == "Production":
             session.close()
-        
-        return 
+
         
