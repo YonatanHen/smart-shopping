7e9cb638274ea10da48fdb7b8172231e538142bb
YehonatanHen <yonatan.h333@gmail.com>
2024-09-24 11:08:11 +0300

    Removed files + created table

---
+++ client/src/components/ListTable.js
@@ -0,0 +1,43 @@
+import '../App.css';
+import Table from 'react-bootstrap/Table';
+
+function ListTable() {
+  return (
+    <div className="list-table">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>List ID</th>
+            
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Larry</td>
+            <td>Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default ListTable;
