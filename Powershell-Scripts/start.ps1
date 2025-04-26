ebba5bc1ed15ea3584ae9565d9434cb679174f19
Yonatan Hen <yonatan.h333@gmail.com>
2025-04-26 17:12:51 +0300

    Created the add item to list feature

---
+++ Powershell-Scripts/start.ps1
@@ -0,0 +1,6 @@
+docker start smart_shopping_db
+cd server
+.\venv\Scripts\activate
+python .\app.py
+cd ../client
+npm start
--- client/src/components/CreateListPageComponenets/createList.js
+++ client/src/components/CreateListPageComponenets/createList.js
@@ -2,15 +2,30 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import ItemsList from './ItemsList';
 
 function CreateList() {
   const [productInput, setProductInput] = useState("");
-  const [amountInput, setAmountInput] = useState("");
+  const [amountInput, setAmountInput] = useState(0);
   const [currentList, editCurrentList] = useState([])
 
   const handleProductInput = (event) => setProductInput(event.target.value);
   const handleAmountInput = (event) => setAmountInput(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (productInput.trim() !== "" && amountInput.trim() !== "") {
+      editCurrentList(prevList => [
+        ...prevList,
+        { item_name: productInput.trim(), amount: amountInput.trim() }
+      ]);
+
+      setProductInput("");
+      setAmountInput("");
+    }
+  }
 
   return (
     <div>
@@ -27,16 +42,22 @@ function CreateList() {
             />
           </Col>
           <Col xs={3}>
-            <Form.Control
-              type="text"
-              placeholder="Amount"
+            <Form.Select
               value={amountInput}
               onChange={handleAmountInput}
-            />
+            >
+              <option value={amountInput}>Amount</option>
+              {Array.from({ length: 20 }, (_, i) => i + 1).map(number => (
+                <option key={number} value={number}>
+                  {number}
+                </option>
+              ))}
+            </Form.Select>
           </Col>
+          <Col xs={1}><Button onClick={(event) => handleSubmit(event)} variant="secondary" size='lg' disabled={productInput.trim() === "" || amountInput === 0}>+</Button></Col>
         </Form.Group>
       </Form>
-      <ItemsList currentListData={currentList} editCurrentList={editCurrentList}/>
+      <ItemsList currentListData={currentList} editCurrentList={editCurrentList} />
     </div>
   );
 }
